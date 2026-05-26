3039681159ea10c7ae8072dcb8920cde1bad25af
Przemysław Tomala <timuslala@gmail.com>
2026-05-26 12:52:00 +0000

    added better waiting for model start

--- run_model.sh
+++ run_model.sh
@@ -16,7 +16,13 @@ SERVER_PID=$!
 
 echo "Waiting for server..."
 
-sleep 120
+echo "Waiting for vLLM to be ready..."
+
+for i in {1..120}; do
+    curl -s http://localhost:8000/v1/models && break
+    echo "not ready yet... $i"
+    sleep 10
+done
 
 python benchmark.py
 
